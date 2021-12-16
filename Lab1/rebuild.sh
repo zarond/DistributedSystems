b8cf13a82490fcb7c91b47a620e313c6faf2ac96
zarond <55189420+zarond@users.noreply.github.com>
2021-12-17 01:59:14 +0300

    Update rebuild.sh

--- Lab1/rebuild.sh
+++ Lab1/rebuild.sh
@@ -12,15 +12,15 @@ docker build --pull --rm -f "Dockerfile" -t stats:latest "."
 cd ..
 
 
-docker run -d --name redis-main --restart=unless-stopped -p 6379:6379 redis:latest
+docker run -d --name redis-main --restart=always -p 6379:6379 redis:latest
 docker run -d -p 5672:5672 --name messagebrocker rabbitmq:latest
 
 #sleep 10
 
-docker run -d -p 8080:8080 --restart=unless-stopped --name gateway gateway:latest
-docker run -d -e "FLAGS= --redis 172.17.0.1:6379" --restart=unless-stopped --name worker1 worker:latest
-docker run -d -e "FLAGS= --redis 172.17.0.1:6379" --restart=unless-stopped --name worker2 worker:latest
-docker run -d --restart=unless-stopped --name stats stats:latest
+docker run -d -p 8080:8080 --restart=always --name gateway gateway:latest
+docker run -d -e "FLAGS= --redis 172.17.0.1:6379" --restart=always --name worker1 worker:latest
+docker run -d -e "FLAGS= --redis 172.17.0.1:6379" --restart=always --name worker2 worker:latest
+docker run -d --restart=always --name stats stats:latest
 
 # wait until all running
 
